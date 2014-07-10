e965f8afe02ef502a26a5273ec0876da86951c94
JenGates3255 <Jengates3255@gmail.com>
2014-07-10 11:23:02 -0600

    end of day

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function() {
 // undo last action (undo delete)
- var $undo = $('.undoDelete');
+var $undo = $('.undoDelete');
  	$undo.on('click', function(){
  		if (lastAction.length){
  			var callback = lastAction.pop();    
@@ -8,54 +8,116 @@ $(document).on('ready', function() {
  		// (modifies original array )
  			callback();
  		}
- });
- var lastAction = [];
 
- var newQuote = function(quote, author){
-//concatinate two elements into one var - creating a new <p> within the dom 
-// adding author and quote
-// 
+});
+ var lastAction = [];
+ var randomQuoteArray = [];
+
+
+
+var newQuote = function(quote, author){
+ //concatinate two elements into one var - creating a new <p> within the dom 
+ // adding author and quote
+ // 
  //1 way to concatenate 	
  	//var element = $("<p>" + quote + " - " + author + " <button> delete </button></p>");
  //2 way to string concatenate  (join multiple strings)
- 	var $element = $("<p>").text(quote + " - " + author);
+ 	var fullQuote = quote + " -" + author;
+ 	var $element = $("<button>", {
+ 		'class': 'element',
+ 		'data-author': author,
+ 		'data-quote': quote }
+ 		).text(fullQuote);
  //	
  //3 way to string concatenate
+ //
+ //
+// select author on click 
+$element.on('click', function(){
+	var author = $(this).attr('data-author');
+	// Attribute selector: .element[attributeName="value"]
+	var $quotes = $('.element[data-author="' + author + '"]');
+
+// if author is = to 'this(clicked)'
+	// for (var i = 0; i < quotes.length; i++) {
+	// 		alert(quotes.eq(i).attr('data-quote'));
+	// };
+	var quotes = [];
+	$quotes.each(function (index, element) {
+		quotes.push($(element).attr('data-quote'));
+	});
+
+	console.log(quotes);
+	
+});
+// 
+// alert in seperate window
  //
  	var $button = $("<button>");
  		$button.text("delete");
  		$element.append($button);
 
-// upon clicking delete button - hide
+ 	// var $stars = $('<span>', { class: 'stars'});
+ 	// var $star =	$('<span>', { class: 'star' }).text('o');
+ 	    
+ 	//  for (var i = 0; i < 5; i++) {
+ 	//  	var $clone = $star.clone();
+ 	//  		$clone.on('click',function(){
+ 	//     	var prior_stars = $stars.find('.star').splice(0, i);
+ 	//     	$(prior_stars).addClass("selected").text('*');
+ 	//     });
+ 	//  	$stars.append($clone);
+ 	//  };
+
+ 	// $element.append($stars); 
+
+ 		
+ // upon clicking delete button - hide
  		$button.on('click',function() {
  			$element.hide();
-
-// upon selecting lastAction(Array of last inputs)
+ 			// Remove it from random quote
+ 			var index = randomQuoteArray.indexOf(fullQuote);
+ 			randomQuoteArray.splice(index, 1);
+ // upon selecting lastAction(Array of last inputs)
  			lastAction.push(function(){
  				$element.show();
- 			})
- 			
- 		})
+ 				randomQuoteArray.push(fullQuote);
+ 			});
+ 		});
 
+	// Add element to page
  	 $(".secondBody").append($element);
- }; 
 
+ 	 // Add element to random array
+ 	 randomQuoteArray.push(fullQuote);
+}; 
+
+$('#random').on('click', function(){
+	var randomQuote = randomQuoteArray[Math.floor(Math.random()*randomQuoteArray.length)];
+	alert(randomQuote);
+});
+
+// 
+// 
 	newQuote("hello", "world");
+	newQuote('My name is Zoey', 'Zoey');
+	newQuote('I am black and furry', 'Zoey');
 
-	$('form').on('submit', function (event) {
-		event.preventDefault();
+$('form').on('submit', function (event) {
+	event.preventDefault();
 
-		var $quote = $(".quote");
-		var $author = $(".author");
-		var quote = $quote.val();
-		var author = $author.val();
+	var $quote = $(".quote");
+	var $author = $(".author");
+	var quote = $quote.val();
+	var author = $author.val();
 
-		newQuote(quote, author);
+	newQuote(quote, author);
 
-		// now clear the fields
-		$quote.val('');
-		$author.val('');
-	});
+	// now clear the fields
+	$quote.val('');
+	$author.val('');
 
+   
+});
 
 });
